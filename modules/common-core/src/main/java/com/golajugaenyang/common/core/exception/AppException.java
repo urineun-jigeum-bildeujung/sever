fd9ca4d323d7eefb15d5baa5b28682b4a83f6160
muncool39 <muncool39@gmail.com>
2026-08-25 17:16:25 +0900

    feat(common-core): 공통 예외 모델 정의

---
+++ modules/common-core/src/main/java/com/golajugaenyang/common/core/exception/AppException.java
@@ -0,0 +1,22 @@
+package com.golajugaenyang.common.core.exception;
+
+
+import lombok.Getter;
+
+
+@Getter
+public class AppException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+}
