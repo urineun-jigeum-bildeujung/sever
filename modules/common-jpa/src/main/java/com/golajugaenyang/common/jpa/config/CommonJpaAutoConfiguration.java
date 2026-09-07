276cb336bfd64ee85492ca5ac4075fffa67115b2
muncool39 <muncool39@gmail.com>
2026-09-07 12:22:52 +0900

    feat(common-jpa): JPA Auditing 설정 및 BaseTimeEntity 추가

---
+++ modules/common-jpa/src/main/java/com/golajugaenyang/common/jpa/config/CommonJpaAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.golajugaenyang.common.jpa.config;
+
+
+import jakarta.persistence.EntityManager;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@AutoConfiguration
+@ConditionalOnClass(EntityManager.class)
+@EnableJpaAuditing
+public class CommonJpaAutoConfiguration {
+
+}
---
+++ modules/common-jpa/src/main/java/com/golajugaenyang/common/jpa/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.golajugaenyang.common.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+}
