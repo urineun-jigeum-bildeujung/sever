bf8381c983bd78c2ba530855f070519fe255a483
muncool39 <muncool39@gmail.com>
2026-09-14 02:13:44 +0900

    fix(product): scheduling 활성화

--- services/product-service/src/main/java/com/golajugaenyang/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/golajugaenyang/product/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.golajugaenyang.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan
 public class ProductServiceApplication {
     public static void main(String[] args) {
