1ba8e30f0247dfd4e5aabe5e876ddee862f81784
jinjinjala-ish <qhshqhsh3124287@gmail.com>
2026-09-14 10:51:21 +0900

    feat(common-security): X-Auth-Id 헤더용 리졸버와 에러코드 추가

---
+++ modules/common-security/src/main/java/com/golajugaenyang/common/security/error/CommonSecurityErrorCode.java
@@ -0,0 +1,21 @@
+package com.golajugaenyang.common.security.error;
+
+import com.golajugaenyang.common.core.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonSecurityErrorCode implements ErrorCode {
+
+    MISSING_AUTH_ID(
+            HttpStatus.UNAUTHORIZED,
+            "SECURITY_401_MISSING_AUTH_ID", "인증 정보가 없습니다."
+    );
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
