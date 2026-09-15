2fc2d9030011a6b5794900453fd3030947c6a9a0
jinjinjala-ish <qhshqhsh3124287@gmail.com>
2026-09-16 00:41:56 +0900

    fix(member): 배송지 entity 수정

--- services/member-service/src/main/java/com/golajugaenyang/member/domain/entity/Address.java
+++ services/member-service/src/main/java/com/golajugaenyang/member/domain/entity/Address.java
@@ -9,25 +9,29 @@ public class Address {
     private Long id;
     private String addressName;
     private String receiver;
+    private String receiverPhone;
+    private String zipCode;
     private String address;
     private String addressDetail;
     private boolean isDefault;
-    private String deliveryNode;
+    private String deliveryNote;
     private Instant createdAt;
     private Instant updatedAt;
     private Long memberId;
 
-    public Address(Long id, String addressName, String receiver, String address,
-        String addressDetail,
-        boolean isDefault, String deliveryNode, Instant createdAt, Instant updatedAt,
+    public Address(Long id, String addressName, String receiver, String receiverPhone,
+        String zipCode, String address, String addressDetail,
+        boolean isDefault, String deliveryNote, Instant createdAt, Instant updatedAt,
         Long memberId) {
         this.id = id;
         this.addressName = addressName;
         this.receiver = receiver;
+        this.receiverPhone = receiverPhone;
+        this.zipCode = zipCode;
         this.address = address;
         this.addressDetail = addressDetail;
         this.isDefault = isDefault;
-        this.deliveryNode = deliveryNode;
+        this.deliveryNote = deliveryNote;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.memberId = memberId;
