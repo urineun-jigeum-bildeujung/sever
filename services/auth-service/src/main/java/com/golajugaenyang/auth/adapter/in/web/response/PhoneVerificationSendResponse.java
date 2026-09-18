6ccd0a303cccd4f6e12c2fb8f68d2fc9aa87b86b
jinjinjala-ish <qhshqhsh3124287@gmail.com>
2026-09-18 12:43:31 +0900

    fix(auth): 전화번호 인증 response 유효시간 타입 API 명세서와 일치하게 변경

--- services/auth-service/src/main/java/com/golajugaenyang/auth/adapter/in/web/response/PhoneVerificationSendResponse.java
+++ services/auth-service/src/main/java/com/golajugaenyang/auth/adapter/in/web/response/PhoneVerificationSendResponse.java
@@ -1,5 +1,5 @@
 package com.golajugaenyang.auth.adapter.in.web.response;
 
-public record PhoneVerificationSendResponse(int expiresIn) {
+public record PhoneVerificationSendResponse(int expiresInSeconds) {
 
 }
