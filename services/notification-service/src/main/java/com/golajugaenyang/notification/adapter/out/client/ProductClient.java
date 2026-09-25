9023cc19af573a03bb444a4c400804fe92136b95
youngsik <dudtlr9308@gmail.com>
2026-09-25 15:22:18 +0900

    fix: enable mTLS for notification product client

--- services/notification-service/src/main/java/com/golajugaenyang/notification/adapter/out/client/ProductClient.java
+++ services/notification-service/src/main/java/com/golajugaenyang/notification/adapter/out/client/ProductClient.java
@@ -1,11 +1,16 @@
 package com.golajugaenyang.notification.adapter.out.client;
 
+import com.golajugaenyang.common.security.config.InternalFeignClientConfig;
 import com.golajugaenyang.notification.adapter.out.client.dto.TimeDealListResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product-service", url = "${product-service.url}")
+@FeignClient(
+    name = "product-service",
+    url = "${product-service.url}",
+    configuration = InternalFeignClientConfig.class
+)
 public interface ProductClient {
 
     @GetMapping("/api/v1/time-deals")
